f85850c4fd92911885113cd650957cd7487e7aea
Dominiq'ue Washington <Ddw109@wiu.edu>
2019-08-30 12:04:04 -0500

    initial Commit

---
+++ CodeChallenge/CodeChallengeDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeChallenge
+{
+    public class CodeChallengeDBContext : DbContext
+    {
+        public CodeChallengeDBContext(DbContextOptions<CodeChallengeDBContext> context):base(context)
+        {
+
+        }
+    }
+
+    //public DbSet<Workshop> Workshops { get; set; }
+}
---
+++ CodeChallenge/Models/CodeChallengeDM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeChallenge.Models
+{
+    public class CodeChallengeDM
+    {
+        public int id { get; set; }
+        public string buildingCode { get; set; }
+        public string description { get; set; }
+        //public enum currentStatus { get; set; }
+        public string createdBy { get; set; }
+        public DateTime createdDate { get; set; }
+        public string lastModifiedBy { get; set; }
+        public DateTime lastUpdatedBy { get; set; }
+    }
+
+    public enum currentStatus
+    {
+
+    }
+}
